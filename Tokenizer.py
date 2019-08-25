c224e8b90df02fcdc18bcdb77acfa7578c8122e6
Pranjali Ingole <pranjali.ingole@students.iiit.ac.in>
2019-08-25 13:10:51 +0530

    Create Index completed (Index size = 70mb)

--- Tokenizer.py
+++ Tokenizer.py
@@ -2,11 +2,13 @@ from nltk.tokenize import word_tokenize
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
+import json
 
 ## Class object will store field wise tokens for one WikiPage
 class TokenObject():
 
     def __init__(self):
+        self.id = 0
         self.title = []
         self.infobox = []
         self.category = []
@@ -22,11 +24,16 @@ tokenizer = RegexpTokenizer(r'\w+')
 ps = PorterStemmer()
 ## List of tokenobjects <--> List of pages
 TokenPages = []
+## Inverted Index Data Structure
+InvIndex = {}
+## Data Structure with mapping of PageID and PageTitle for search results
+Page_ID_Title = {}
 
 ## Input : Filtered parsed data file
 def word_tokenizer(IPFilePath):
 
     Tokenobj = TokenObject()
+    PageID = 0
 
     InfoboxText = ""
     CategoryText = ""
@@ -74,6 +81,9 @@ def word_tokenizer(IPFilePath):
                     RefFlag = False
 
                 ## Continue reading next line after processing for this title line
+                PageID = PageID + 1
+                Tokenobj.id = PageID
+                Page_ID_Title[PageID] = line[15:]
                 Tokenobj.title = tokenizer.tokenize(line[15:])
                 continue
 
@@ -143,26 +153,38 @@ def word_tokenizer(IPFilePath):
 
     TokenPages.append(Tokenobj)
 
+def CaseFolding():
+
+    for Tokenobj in TokenPages:
+        Tokenobj.title = [w.casefold() for w in Tokenobj.title]
+        Tokenobj.infobox = [w.casefold() for w in Tokenobj.infobox]
+        Tokenobj.category = [w.casefold() for w in Tokenobj.category]
+        Tokenobj.links = [w.casefold() for w in Tokenobj.links]
+        Tokenobj.ref = [w.casefold() for w in Tokenobj.ref]
+        Tokenobj.body = [w.casefold() for w in Tokenobj.body]
+
+
 def StopWordRemoval():
 
-    infobox_stopwords = ['Infobox', 'infobox']
-    category_stopwords = ['Category', 'category']
-    links_stopwords = ['External links']
-    ref_stopwords = ['Reflist', 'reflist', 'Bibliography', 'bibliography']
-    body_stopwords = ['REDIRECT']
+    infobox_stopwords = ['infobox']
+    category_stopwords = ['category']
+    links_stopwords = ['external links']
+    ref_stopwords = ['reflist','bibliography']
+    body_stopwords = ['redirect']
 
     ## Stopword removal, Numbers removal and case folding
     for Tokenobj in TokenPages:
-        Tokenobj.title = [w.casefold() for w in Tokenobj.title]
-        Tokenobj.infobox = [w.casefold() for w in Tokenobj.infobox if w not in infobox_stopwords and w not in stop_words and w.isalpha()]
-        Tokenobj.category = [w.casefold() for w in Tokenobj.category if w not in category_stopwords and w not in stop_words and w.isalpha()]
-        Tokenobj.links = [w.casefold() for w in Tokenobj.links if w not in links_stopwords and w not in stop_words and w.isalpha()]
-        Tokenobj.ref = [w.casefold() for w in Tokenobj.ref if w not in ref_stopwords and w not in stop_words and w.isalpha()]
-        Tokenobj.body = [w.casefold() for w in Tokenobj.body if w not in body_stopwords and w not in stop_words and w.isalpha()]
+        Tokenobj.title = [w for w in Tokenobj.title if w not in stop_words and w.isalpha()]
+        Tokenobj.infobox = [w for w in Tokenobj.infobox if w not in infobox_stopwords and w not in stop_words and w.isalpha()]
+        Tokenobj.category = [w for w in Tokenobj.category if w not in category_stopwords and w not in stop_words and w.isalpha()]
+        Tokenobj.links = [w for w in Tokenobj.links if w not in links_stopwords and w not in stop_words and w.isalpha()]
+        Tokenobj.ref = [w for w in Tokenobj.ref if w not in ref_stopwords and w not in stop_words and w.isalpha()]
+        Tokenobj.body = [w for w in Tokenobj.body if w not in body_stopwords and w not in stop_words and w.isalpha()]
 
 def Stemming():
 
     for Tokenobj in TokenPages:
+        Tokenobj.title = [ps.stem(w) for w in Tokenobj.title]
         Tokenobj.infobox = [ps.stem(w) for w in Tokenobj.infobox]
         Tokenobj.category = [ps.stem(w) for w in Tokenobj.category]
         Tokenobj.links = [ps.stem(w) for w in Tokenobj.links]
@@ -185,10 +207,53 @@ def PrintTokens():
         print("BODY TOKENS")
         print(Tokenobj.body)
 
+def Check_Index(word, doc_id):
+
+    if word not in InvIndex.keys():
+        InvIndex[word] = {}
+    if doc_id not in InvIndex[word].keys():
+        InvIndex[word][doc_id] = [0,0,0,0,0,0]
+
+def Create_Index():
+
+    for TokenObj in TokenPages:
+
+        for w in TokenObj.title:
+            Check_Index(w, TokenObj.id)
+            InvIndex[w][TokenObj.id][0] = InvIndex[w][TokenObj.id][0] + 1
+
+        for w in TokenObj.infobox:
+            Check_Index(w, TokenObj.id)
+            InvIndex[w][TokenObj.id][1] = InvIndex[w][TokenObj.id][1] + 1
+
+        for w in TokenObj.category:
+            Check_Index(w, TokenObj.id)
+            InvIndex[w][TokenObj.id][2] = InvIndex[w][TokenObj.id][2] + 1
+
+        for w in TokenObj.links:
+            Check_Index(w, TokenObj.id)
+            InvIndex[w][TokenObj.id][3] = InvIndex[w][TokenObj.id][3] + 1
+
+        for w in TokenObj.ref:
+            Check_Index(w, TokenObj.id)
+            InvIndex[w][TokenObj.id][4] = InvIndex[w][TokenObj.id][4] + 1
+
+        for w in TokenObj.body:
+            Check_Index(w, TokenObj.id)
+            InvIndex[w][TokenObj.id][5] = InvIndex[w][TokenObj.id][5] + 1
+
+def Store_Index():
+    with open('index.json', 'w') as file:
+     file.write(json.dumps(InvIndex))
+
 word_tokenizer("Phase_1_Result.xml")
 print("TOKENIZATION DONE")
+CaseFolding()
 StopWordRemoval()
 print("STOP WORD REMOVAL AND CASE FOLDING DONE")
 Stemming()
 print("STEMMING DONE")
-PrintTokens()
+Create_Index()
+print("INDEX CREATION DONE")
+Store_Index()
+print("INDEX STORED")
